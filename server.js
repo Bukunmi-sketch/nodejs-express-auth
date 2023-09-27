d9b9d7b3c1587803109c4b1afaf569ee5a6e73b5
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-09-27 08:48:38 +0100

    updated

--- server.js
+++ server.js
@@ -11,4 +11,27 @@ var corsOption = {
 
 app.use(cors(corsOption));
 
+// parse requests of content-type - application/json
 app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({extended : true }));
+
+app.use(
+    cookieSession({
+        name:"bukunmi olarinde",
+        keys:["COOKIE_SECRET"],
+        httpOnly: true
+    })
+);
+
+
+//simple route
+app.get("/", (req,res)=> {
+    res.json({message: "welcome to buuczzi application"})
+})
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);
+})
